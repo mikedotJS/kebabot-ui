969c84bd5bb84976891e3515d8b1dbfea734c05d
Michaël Romain <mike@MacBook-Pro-de-Michael.home>
2021-08-26 14:39:46 +0200

    Add login page

---
+++ src/components/Login.tsx
@@ -0,0 +1,109 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Link,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { Field, Form, Formik } from "formik";
+import React from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
+
+interface LoginFormValues {
+  email: string;
+  password: string;
+}
+
+function Login(): JSX.Element {
+  const handleSubmit = (values: LoginFormValues, actions) => {
+    setTimeout(() => {
+      console.log("submit", values);
+      actions.setSubmitting(false);
+    }, 1000);
+  };
+
+  return (
+    <Flex justifyContent="center" flexDir="column" alignItems="center" p="8">
+      <VStack spacing="16">
+        <Text fontSize="3xl" fontWeight="bold">
+          Kebabot
+        </Text>
+        <VStack>
+          <Text fontSize="4xl" fontWeight="bold">
+            Sign in to your account
+          </Text>
+          <Text>
+            Don't have an account?{" "}
+            <Link as={ReactRouterLink} to="/signup" color="teal.500">
+              Create one
+            </Link>
+          </Text>
+        </VStack>
+      </VStack>
+      <Box bg="gray.700" p="8" borderRadius="md" w="lg" mt="8">
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={handleSubmit}
+        >
+          {(props) => (
+            <Form>
+              <VStack spacing="4">
+                <Field name="email">
+                  {({ field, form }) => (
+                    <FormControl
+                      id="email"
+                      isInvalid={form.errors.email && form.touched.email}
+                    >
+                      <FormLabel htmlFor="email">Email address</FormLabel>
+                      <Input
+                        {...field}
+                        type="email"
+                        placeholder="Enter your email address"
+                      />
+                      <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                      <FormHelperText>
+                        We'll never share your email.
+                      </FormHelperText>
+                    </FormControl>
+                  )}
+                </Field>
+
+                <Field name="password">
+                  {({ field, form }) => (
+                    <FormControl
+                      id="password"
+                      isInvalid={form.errors.password && form.touched.password}
+                    >
+                      <FormLabel htmlFor="password">Password</FormLabel>
+                      <Input {...field} type="password" placeholder="••••••" />
+                      <FormErrorMessage>
+                        {form.errors.password}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+
+                <Button
+                  isLoading={props.isSubmitting}
+                  type="submit"
+                  w="full"
+                  bg="teal.500"
+                >
+                  Sign in
+                </Button>
+              </VStack>
+            </Form>
+          )}
+        </Formik>
+      </Box>
+    </Flex>
+  );
+}
+
+export default Login;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { Layout } from "./components/Layout";
 import React from "react";
 import ReactDom from "react-dom";
 import { extendTheme } from "@chakra-ui/react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
@@ -12,7 +14,18 @@ const theme = extendTheme({ config });
 
 const App = () => (
   <ChakraProvider theme={theme}>
-    <Layout />
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Layout />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   </ChakraProvider>
 );
 
