a56bb7e50c0c0778ad4c7872a1ba2daf4537cffc
AServantDev <arnaud.github@gmail.com>
2021-09-17 19:38:40 +0200

    add reaction roles page and text area

--- src/components/Reactionroles.tsx
+++ src/components/Reactionroles.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiPlusCircle } from 'react-icons/fi';
 
-export default function Reactionroles(): JSX.Element {
-  return <div>Bite</div>;
-}
+import { Box, Flex, IconButton, Input, Textarea, VStack } from '@chakra-ui/react';
+
+export const Reactionroles = (): JSX.Element => {
+  return (
+    <Box>
+      <VStack>
+        <Textarea>Hi there</Textarea>
+        <Input placeholder="Role Name" size="sm" />
+      </VStack>
+    </Box>
+  );
+};
