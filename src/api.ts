5d13f37e6e0be8f244db4033f99557ea511278dc
Mike Romain <mchlrmn@me.com>
2021-12-19 20:05:10 +0100

    Use env to determine api url

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,10 @@ import axios from "axios";
 const auth = JSON.parse(localStorage.getItem("auth"));
 
 export default axios.create({
-  baseURL: "http://localhost:3333/api",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? process.env.API_URL_DEV
+      : process.env.API_URL_PROD,
   headers: {
     Authorization: `Bearer ${auth?.token || ""}`,
   },
